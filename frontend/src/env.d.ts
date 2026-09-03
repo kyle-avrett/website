85dfb369976d967645d77da779cfeb7c6bb610c8
Kyle Avrett <kyle@upsurge.consulting>
2026-09-03 12:36:11 -0500

    Added analytics

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
     readonly SITE_URL?: string;
     readonly CI_SKIP_CONTENT_COLLECTIONS?: string;
     readonly CI_SKIP_RSS_SITEMAP?: string;
+    readonly ANALYTICS_SCRIPT?: string;
+    readonly ANALYTICS_SITE_ID?: string;
     readonly PUBLIC_GISCUS_ENABLED?: string;
     readonly PUBLIC_GISCUS_REPO?: string;
     readonly PUBLIC_GISCUS_REPO_ID?: string;
