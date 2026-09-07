d474b0109da8795f35f960dc634b5dd31d6cac9c
Kyle Avrett <kyle@upsurge.consulting>
2026-09-07 14:09:27 -0500

    Added mdx

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -2,9 +2,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+
+    integrations: [mdx()],
 });
